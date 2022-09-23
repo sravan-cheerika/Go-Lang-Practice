47f92c4640cae2e00003b6f2ab610cc1c7dbb63b
sravan-cheerika <113576330+sravan-cheerika@users.noreply.github.com>
2022-09-23 10:20:05 +0530

    Update 6Assignment.go

--- Assignments/6Assignment.go
+++ Assignments/6Assignment.go
@@ -10,3 +10,30 @@ Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 */
 
+package main
+
+import (
+	"fmt"
+)
+
+var nums = []int{0, 1, 2, 3, 4, 6, 8, 9}
+var target int = 9
+
+func twoSum(nums []int, target int) (int, int) {
+	if len(nums) <= 1 {
+		return 0, 0
+	}
+	hdict := make(map[int]int)
+	for i := 1; i < len(nums); i++ {
+		if val, ok := hdict[nums[i+1]]; ok {
+			return val, i + 1
+		} else {
+			hdict[target-nums[i+1]] = i + 1
+		}
+	}
+	return 0, 0
+}
+
+func main() {
+	fmt.Println(twoSum(nums, target))
+}
